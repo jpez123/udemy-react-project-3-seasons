d9f0224db46d6563ba90951b48048af5f1620009
John Zhang <zhangj1010@Johns-MacBook-Pro.local>
2019-08-10 17:16:13 -0400

    Initialize project - class components

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,12 @@
+//Import libraries
+import React from 'react';
+
+//Component
+const SeasonDisplay = () => {
+    return (
+        <div>SeasonDisplay</div>
+    );
+};
+
+//Exports component
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
+//Import libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Class Component
+class App extends React.Component {
+    //Constructor function (initialization)
+    constructor(props) {
+        super(props); //Required by constructor (Ensure parent's constructor is called)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        //Initializes state (only time to direct assigment)
+        this.state = { 
+            lat: null,
+            errorMsg: ''
+        };
+
+        //Geolocation (callback - success, failure)
+        window.navigator.geolocation.getCurrentPosition(
+            //Call setState to change position
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMsg: err.message });
+            }
+        );
+    }
+
+    //Component Lifecycle
+    componentDidMount() {
+        console.log('Mounted');
+    }
+
+    componentDidUpdate() {
+        console.log('Re-rendered');
+    }
+
+    //Render Method (required)
+    render() {
+        //Renders state onto DOM (conditional)
+        if (this.state.errorMsg && !this.state.lat) {
+           return  <div>Error: { this.state.errorMsg }</div>;
+        } else if (!this.state.errorMessage && this.state.lat) {
+            return <div>Lattitude: { this.state.lat } </div>;
+        } else {
+            return <div>Loading...</div>;
+        }
+    }
+}
+
+//Renders it to DOM
+ReactDOM.render(<App />, document.querySelector('#root'));
